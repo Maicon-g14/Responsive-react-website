b69e522e355ba617a992ee1d1ac86c3124d9efa8
Maicon-g14 <maicon_gabriel12@hotmail.com>
2023-05-03 21:11:10 -0300

    fixacao e correcoes da navbar

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -1,46 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
+import Logo from "../images/green-acres-favicon2.png";
 
 
 function Navbar() {
     return (
-        <div data-bs-theme="dark" className='bg-dark shadow'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-12'>
-                        <nav className='navbar navbar-expand-lg'>
-                            <div className='container-fluid'>
-                                <Link to='/' className='navbar-brand'>Green Acres</Link>
-                                
-                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon" />
-                                </button>
-                                
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                                        <li class="nav-item">
-                                            <Link to='/' className='nav-link active'>Home</Link>
-                                        </li>
-                                        <li class="nav-item">
-                                            <Link to='/about' className='nav-link active'>About</Link>
-                                        </li>
-                                        <li class="nav-item">
-                                            <Link to='/' className='nav-link active'>Buy</Link>
-                                        </li>
-                                        <li class="nav-item">
-                                            <Link to='/' className='nav-link active'>Blog</Link>
-                                        </li>
-                                        <li class="nav-item">
-                                            <Link to='/contact' className='nav-link active'>Contact us</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
+        <nav data-bs-theme="dark" className='navbar sticky-top navbar-expand-lg bg-dark shadow'>
+            <div className='container-fluid col-md-11'>
+                <Link to='/' className='navbar-brand'>
+                    <img src={Logo} className='img-fluid' alt='Green Acres Logo' />
+                </Link>
+                
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 navbar-nav-scroll">
+                        <li className="nav-item">
+                            <Link to='/' className='nav-link active'>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/about' className='nav-link active'>About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-link active'>Buy</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-link active'>Blog</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/contact' className='nav-link active'>Contact us</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
--- src/components/inc/Slider.js
+++ src/components/inc/Slider.js
@@ -4,34 +4,34 @@ import Slide2 from "../images/slide2.png";
 
 function Slider() {
     return (
-        <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <div id="carouselExampleCaptions" className="carousel slide">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src={Slide1} class="d-block w-100" alt="..." />
-                    <div class="carousel-caption d-none d-md-block">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src={Slide1} className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
                         <h5>Green Acres</h5>
                         <p>Grow it like you mean it</p>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <img src={Slide2} class="d-block w-100" alt="..." />
-                    <div class="carousel-caption d-none d-md-block">
+                <div className="carousel-item">
+                    <img src={Slide2} className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
                         <h5>Green Acres</h5>
                         <p>Grow it like you mean it</p>
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     );
